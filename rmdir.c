adca206684171736fa3dc6b1ca7e6d8b4a84b773
AbdiDewax86 <60722044+AbdiDewax86@users.noreply.github.com>
2020-05-08 17:12:35 +0700

    Update rmdir.c

--- rmdir.c
+++ rmdir.c
@@ -10,27 +10,65 @@ main(int argc, char *argv[])
   struct stat st;
 
   if(argc < 2){
-    printf(2, "Usage: rmdir directory...\n");
+    printf(2, "Usage: rmdir [directory] or rmdir [argument] [directory]\n");
     exit();
   }
 
-  for(i = 1; i < argc; i++){
-    fd = open(argv[i], O_RDONLY);
+  if(argc == 2){
+    fd = open(argv[1], O_RDONLY);
     
     fstat(fd,&st);
 
     if(st.type == T_DIR){
-      if(unlink(argv[i]) < 0){
-        printf(2, "rmdir: %s failed to delete directory\n", argv[i]);
-        break;
+      if(unlink(argv[1]) < 0){
+        printf(2, "rmdir: %s failed to delete directory\n", argv[1]);
       }
       else
       {
-        printf(2, "rmdir: %s directory deleted\n",argv[i]);
+        printf(2, "rmdir: %s directory deleted\n",argv[1]);
       }
     }
     else{
-      printf(2, "rmdir: %s is not a directory\n", argv[i]);
+      printf(2, "rmdir: %s is not a directory\n", argv[1]);
+    }
+  }
+  else{
+    char * argument = argv[1];
+    char * path = argv[2];
+    if(argument[0]=='-' && argument[1]=='p'){
+      while(strlen(path) != 0){
+        fd = open(path, O_RDONLY);
+    
+        fstat(fd,&st);
+
+        if(st.type == T_DIR){
+          if(unlink(argv[2]) < 0){
+            printf(2, "rmdir: %s failed to delete directory\n", argv[2]);
+            break;
+          }
+          else
+          {
+            printf(2, "rmdir: %s directory deleted\n",argv[2]);
+          }
+        }
+        else{
+          printf(2, "rmdir: %s is not a directory\n", argv[2]);
+          break;
+        }
+
+        for(i=strlen(path)-1; i>=0; i--){
+          if(path[i]=='/'){
+            path[i]='\0';
+            break;
+          }
+          else{
+            path[i]='\0';
+          }
+        }
+      }
+    }
+    else{
+      printf(2, "unknown argument: %s", argument);
     }
   }
 
